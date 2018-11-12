bf8a7822ef5796ca489a744b8c058aeb39e02003
Saskia <jansen.saskia@gmail.com>
2018-11-12 23:48:43 +0100

    corrected typo and cleaned up comments

--- tgif.js
+++ tgif.js
@@ -24,7 +24,6 @@ var demCheck = document.getElementById("dem-check");
 var indCheck = document.getElementById("ind-check");
 var dropdown = document.getElementById("state-filter");
 var loading = true;
-var fetchUrl = ""
 
 function giveURL() {
     if (location.pathname.indexOf("house") == -1) {
@@ -84,12 +83,9 @@ function fillAttendanceLoyalty() {
         })
 }
 
-function isMembersPage() {
-    return (location.pathname.indexOf("members") != -1);
-}
 
 function getData() {
-    if (isMembersPage()) {
+    if (location.pathname.indexOf("members") != -1) {
         fillMembers();
     } else {
         fillAttendanceLoyalty();
@@ -99,7 +95,7 @@ function getData() {
 getData();
 
 function showpage() {
-    if (isMembersPage()) {
+    if (location.pathname.indexOf("members") != -1) {
         document.getElementById("loading").style.visibility = "hidden";
         document.getElementById("senate-data").style.visibility = "visible";
     } else {
@@ -168,15 +164,7 @@ function addMembers(members) {
 }
 
 function defilter() {
-    //    var arraySelected = Array.from(document.querySelectorAll('input[class="myCheckbox"]:checked')).map(elt => elt.value);
     var everyRow = document.getElementsByClassName("table-row");
-
-    //var arraySelected = event.target.value;
-    //console.log("arraySelected is", arraySelected);
-    //var z = ["R","I","D", "R"];
-    //var partyValue = document.getElementById("FilterForp").value;
-    //var result= z.filter(includedArr);
-    //console.log(result);
     for (var k = 0; k < everyRow.length; k++) {
         everyRow[k].classList.add("hideRow");
         if (everyRow[k].classList.contains("showRow")) {
@@ -185,9 +173,6 @@ function defilter() {
     }
     for (var i = 0; i < everyRow.length; i++) {
         if (dropdown.value == everyRow[i].getAttribute('data-state') || dropdown.value == "all") {
-            //            console.log(dropdown.value)
-            //            console.log(everyRow[i].getAttribute("data-state"))
-            //            console.log(JSON.stringify(members[i].state))
             if (repCheck.checked && everyRow[i].classList.contains("R")) {
                 everyRow[i].classList.add("showRow");
                 if (everyRow[i].classList.contains("hideRow")) {
@@ -217,23 +202,12 @@ function defilter() {
 }
 
 function includedArr(z) {
-    /*var partyValue = document.getElementsByClassName("myCheckbox");
-    var valueArray = [];
-        for (i = 0; i <partyValue.length; i++) {
-         //   partyValue[i].classList
-            valueArray.push(partyValue[i].value) 
-        }
-    //    console.log("valueArray" + valueArray);
-    //    var partyValue = ["R", "D"];
-    */
     var valueArray = Array.from(document.querySelectorAll('input[class="myCheckbox"]:checked')).map(elt => elt.value);
-    //console.log("valueArray is " + valueArray);
     return valueArray.includes(z);
 }
 
 function filterState(opt) {
     var arrSelected = Array.from(document.querySelectorAll('#state-filter option:checked')).map(opt => opt.value);
-    //console.log("arrSelected is " + arrSelected);
 }
 
 function addOptions(members) {
@@ -255,7 +229,6 @@ function addOptions(members) {
     }
     var statesNew = unique(states);
     statesNew.sort();
-    //console.log(statesNew);    
     for (i = 0; i < statesNew.length; i++) {
         var option = document.createElement("option");
         option.innerHTML = statesNew[i];
@@ -264,7 +237,6 @@ function addOptions(members) {
 }
 
 function numbers(members) {
-    //    var members = members.results[0].members
     console.log(members.length)
     for (var i = 0; i < members.length; i++) {
         if (members[i].party == "R") {
@@ -297,15 +269,11 @@ function numbers(members) {
     if (isNaN(obj.perc_voted_w_prty_dem)) obj.perc_voted_w_prty_dem = 0;
     if (isNaN(obj.perc_voted_w_prty_ind)) obj.perc_voted_w_prty_ind = 0;
     if (isNaN(obj.perc_voted_w_prty_total)) obj.perc_voted_w_prty_total = 0;
-    //        console.log(obj);
 
 }
 
 
 function tableRows(members) {
-    //        console.log(obj);
-
-    //   var members = members.results[0].members
     var tableColumns = [];
     for (var i = 0; i < members.length; i++) {
         var name = members[i].name;
@@ -341,19 +309,13 @@ function tableRows(members) {
     var toptenRows = [];
 
     var tenperc = Math.round(0.1 * tableColumns.length);
-    //console.log(s[s.length-tenperc][2]);
 
     for (var i = 0; i < members.length; i++) {
         if ((tableColumns[i].avg == tableColumns[tableColumns.length - tenperc].avg && i >= tableColumns.length - tenperc) || i > tableColumns.length - tenperc) {
-            //console.log(tableColumns[tableColumns.length-tenperc][2]);
-            //console.log(i); 
             toptenRows.push(tableColumns[i]);
         }
         if ((tableColumns[i].avg == tableColumns[tenperc - 1].avg && i <= tenperc - 1) || i < tenperc - 1) {
-            //console.log(tableColumns[i]);
-            //console.log(i);    
             bottomtenRows.push(tableColumns[i]);
-            //            obj.name_most_votes_missed[i]= bottomtenRows.name;
         }
     }
 
@@ -370,8 +332,6 @@ function tableRows(members) {
         return 0;
     });
 
-    //   addNumbers(bottomtenRows, toptenRows);
-    //////////////////////////////////////////////
 
     tableColumns.sort(function (a, b) {
         var valueA, valueB;
@@ -389,27 +349,15 @@ function tableRows(members) {
     var toptenRowsLoyalty = [];
 
     var tenperc = Math.round(0.1 * tableColumns.length);
-    //console.log(s[s.length-tenperc][2]);
-    //    console.log(tenperc);
-    //    console.log(members.length);
-
+    
     for (var i = 0; i < members.length; i++) {
         if ((tableColumns[i].vot_pct == tableColumns[tableColumns.length - tenperc].vot_pct && i >= tableColumns.length - tenperc) || i > tableColumns.length - tenperc) {
-            //console.log(tableColumns[tableColumns.length-tenperc][2]);
-            //console.log(i); 
             toptenRowsLoyalty.push(tableColumns[i]);
         }
         if ((tableColumns[i].vot_pct == tableColumns[tenperc - 1].vot_pct && i <= tenperc - 1) || i < tenperc - 1) {
-            //console.log(tableColumns[i]);
-            //console.log(i);    
             bottomtenRowsLoyalty.push(tableColumns[i]);
-            //            obj.name_most_votes_missed[i]= bottomtenRows.name;
         }
     }
-
-    //    console.log(bottomtenRows[1]);
-    //    console.log(toptenRows.length);
-    //    console.log(Object.keys(bottomtenRows));
 
     toptenRowsLoyalty.reverse(function (a, b) {
         var valueA, valueB;
@@ -423,20 +371,10 @@ function tableRows(members) {
         return 0;
     });
 
-    //    console.log(bottomtenRowsLoyalty);
-    //    console.log(bottomtenRows);
-    //    console.log(toptenRows);
-    //    console.log(toptenRowsLoyalty);
-
     addNumbers(bottomtenRows, toptenRows, bottomtenRowsLoyalty, toptenRowsLoyalty);
 }
 
 function addNumbers(bttmTen, tpTen, bttmTenLoy, tpTenLoy) {
-    //    console.log(obj);
-    //    console.log("bttmTen", bttmTen);
-    //    console.log("bttmTenLoy", bttmTenLoy);
-    //    console.log("tpTen", tpTen);
-    //    console.log("tpTenLoy", tpTenLoy);
     console.log(obj["number_independents"])
     console.log(obj.number_independents)
     var tbl = document.getElementById("glance-senate-data").getElementsByTagName('tbody')[0];
@@ -448,11 +386,9 @@ function addNumbers(bttmTen, tpTen, bttmTenLoy, tpTenLoy) {
     var partyTotal = document.createTextNode("Total");
     td.appendChild(partyTotal);
     tr.appendChild(td);
-    //console.log(document.getElementById("glance-senate-data").rows[1].cells.item(1).innerHTML);
     document.getElementById("glance-senate-data").rows[1].cells.item(1).innerHTML = obj["number_republicans"];
     document.getElementById("glance-senate-data").rows[2].cells.item(1).innerHTML = obj["number_democrats"];
     document.getElementById("glance-senate-data").rows[3].cells.item(1).innerHTML = obj["number_independents"];
-    //        document.getElementById("glance-senate-data").rows[4].cells.item(1).innerHTML=obj["number_total"];
     var td = document.createElement("td");
     var partyTotal = document.createTextNode(obj["number_total"]);
     td.appendChild(partyTotal);
@@ -466,9 +402,7 @@ function addNumbers(bttmTen, tpTen, bttmTenLoy, tpTenLoy) {
     td.appendChild(PercpartyTotal);
     tr.appendChild(td);
 
-    //generate table with the bottom and top ten data for senate attendance page    
-    //if (location.pathname == "/tgif-senate-attendance.html") 
-    
+    //generate table with the bottom and top ten data for senate attendance page        
     if (location.pathname.indexOf("senate-attendance") != -1) {
         generateTenTable(bttmTen, "least-senate-data", "tot", "avg")
         generateTenTable(tpTen, "most-senate-data", "tot", "avg")
@@ -476,30 +410,28 @@ function addNumbers(bttmTen, tpTen, bttmTenLoy, tpTenLoy) {
 
 
     //generate table with the bottom and top ten data for senate loyalty page    
-    /if (location.pathname == "/tgif-senate-loyalty.html") {
-    if (location.pathname.indexOf("senate-loyalty") != -1) 
+    if (location.pathname.indexOf("senate-loyalty") != -1) {
         generateTenTable(bttmTenLoy, "least-senate-data-loyalty", "tot_vot", "vot_pct")
         generateTenTable(tpTenLoy, "most-senate-data-loyalty", "tot_vot", "vot_pct")
     }
 
     //generate table with the bottom and top ten data for house attendance page   
-    //if (location.pathname == "/tgif-house-attendance.html") {
-    if (location.pathname.indexOf("house-attendance") != -1) 
+    if (location.pathname.indexOf("house-attendance") != -1) {
         generateTenTable(bttmTen, "least-house-data", "tot", "avg")
         generateTenTable(tpTen, "most-house-data", "tot", "avg")
     }
 
-
     //generate table with the bottom and top ten data for house loyalty page
-    //if (location.pathname == "/tgif-house-loyalty.html") {
-    if (location.pathname.indexOf("house-loyalty") != -1) 
+    if (location.pathname.indexOf("house-loyalty") != -1){ 
         generateTenTable(bttmTenLoy, "least-house-data-loyalty", "tot_vot", "vot_pct")
         generateTenTable(tpTenLoy, "most-house-data-loyalty", "tot_vot", "vot_pct")
     }
 }
 
+
 function generateTenTable(array, table, varOne, varTwo) {
 
+    console.log("table = " + table);
     var tenTable = document.getElementById(table);
 
     for (i = 0; i < array.length; i++) {
@@ -518,6 +450,6 @@ function generateTenTable(array, table, varOne, varTwo) {
         tenTr.appendChild(td2);
         tenTr.appendChild(td3);
 
-        tenTable.appendChild(tenTr);
+        tenTable.appendChild(tenTr); 
     }
 }
